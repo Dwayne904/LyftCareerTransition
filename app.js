a109974ec14f106dccfd3366abbeb3dbab453aca
Dwayne Wilson <dwayne.wilson84@gmail.com>
2021-04-27 01:14:29 -0500

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const createError = require('http-errors');
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const testRouter = require('./routes/testRouter');
 
 const app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
